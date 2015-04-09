9dad1a28fade48797ce9ce7aa3a8d52e74917f11
Florian MORGAN <florian.morgan@gmail.com>
2015-04-09 22:32:23 +0200

    Added countNeighbours and getNextCellState functions

--- src/main/js/GameOfLife.js
+++ src/main/js/GameOfLife.js
@@ -12,4 +12,34 @@ GameOfLife.prototype.display = function(matrix) {
     return '\n'+matrix.map(displayLine).join('\n');
 };
 
+GameOfLife.prototype.getNextCellState = function(neighbourhood) {
+
+    if(neighbourhood.countNeighbours() >= 2 ) return 1;
+    return 0;
+};
+
+
+GameOfLife.prototype.next = function(matrix) {
+
+
+    /*if(JSON.stringify(matrix) === JSON.stringify([[]])) return matrix;
+    if(JSON.stringify(matrix) === JSON.stringify([[1,0,0], [0,1,0], [1,0,0]])) return [[0,0,0], [0,1,0], [0,0,0]];
+    return [[0,0,0], [0,0,0], [0,0,0]];*/
+};
+
 module.exports = GameOfLife;
+
+
+
+ Array.prototype.countNeighbours = function(){
+    var count = function(accumulator, value){
+                      return accumulator + value;
+               };
+    var countLivingCells = this.map(function(line){
+                               return line.reduce(count,0)
+                        }).reduce(count,0);
+
+    var isCenterCellAlive = this[1][1];
+    return  countLivingCells - (isCenterCellAlive ? 1 : 0);
+
+}
--- src/test/js/GameOfLifeSpec.js
+++ src/test/js/GameOfLifeSpec.js
@@ -15,4 +15,90 @@ describe("GameOfLife", function() {
       expect(gameOfLife.display([[1,0],[0,1]])).toEqual('\n▓▓░░\n░░▓▓');
    });
   });
+
+  describe("getNextCellState", function() {
+    describe("Rule: Any live cell with fewer than two live neighbours dies", function() {
+      it("should return 0 when given 000-010-000", function() {
+        expect(gameOfLife.getNextCellState([[0,0,0],[0,1,0],[0,0,0]])).toEqual(0);
+      });
+
+      it("should return 0 when given 000-110-000", function() {
+        expect(gameOfLife.getNextCellState([[0,0,0],[1,1,0],[0,0,0]])).toEqual(0);
+      });
+    });
+
+    describe("Any live cell with two or three live neighbours lives on to the next generation", function() {
+      it("should return 1 when given 001-011-000", function (){
+        expect(gameOfLife.getNextCellState([[0,0,1],[0,1,1],[0,0,0]])).toEqual(1);
+      });
+      it("should return 1 when given 000-110-011", function (){
+        expect(gameOfLife.getNextCellState([[0,0,0],[1,1,0],[0,1,1]])).toEqual(1);
+      });
+
+    });
+
+ describe("countNeighbours : count the number of neighbours of a neighbourhood", function() {
+      it("should return 0 when given 000-000-000", function(){
+        expect([[0,0,0],[0,0,0],[0,0,0]].countNeighbours()).toEqual(0);
+      });
+      it("should return 0 when given 000-010-000", function(){
+        expect([[0,0,0],[0,1,0],[0,0,0]].countNeighbours()).toEqual(0);
+      });
+       it("should return 1 when given 100-000-000", function(){
+                expect([[1,0,0],[0,0,0],[0,0,0]].countNeighbours()).toEqual(1);
+       });
+       it("should return 1 when given 100-010-000", function(){
+                          expect([[1,0,0],[0,1,0],[0,0,0]].countNeighbours()).toEqual(1);
+                 });
+  it("should return 7 when given 111-011-111", function(){
+                          expect([[1,1,1],[0,1,1],[1,1,1]].countNeighbours()).toEqual(7);
+                 });
+
+
+
+ });
+
+
+
+
+  });
+
+/*  describe("next", function() {
+    it("should return '[[]]' when given [[]]", function() {
+      var previous = [[]];
+      console.log(gameOfLife.display(previous));
+      var next = gameOfLife.next(previous);
+      console.log(gameOfLife.display(next));
+      expect(next).toEqual([[]]);
+    });
+
+    it("should return '[[0,0,0], [0,0,0], [0,0,0]]' when given [[0,0,0], [0,1,0], [0,0,1]]", function() {
+      var previous = [[0,0,0], [0,1,0], [0,0,1]];
+      var expected = [[0,0,0], [0,0,0], [0,0,0]];
+      console.log(gameOfLife.display(previous));
+      var next = gameOfLife.next(previous);
+      console.log(gameOfLife.display(next));
+      expect(next).toEqual(expected);
+    });
+
+    it("should return '[[0,0,0], [0,1,0], [0,0,0]]' when given [[1,0,0], [0,1,0], [1,0,0]]", function() {
+      var previous = [[1,0,0], [0,1,0], [1,0,0]];
+      var expected = [[0,0,0], [0,1,0], [0,0,0]];
+      console.log(gameOfLife.display(previous));
+      var next = gameOfLife.next(previous);
+      console.log(gameOfLife.display(next));
+      expect(next).toEqual(expected);
+    });
+
+    it("should return '[[0,0,0], [0,1,0], [0,0,0]]' when given [[1,0,0], [0,1,0], [0,0,1]]", function() {
+      var previous = [[1,0,0], [0,1,0], [0,0,1]];
+      var expected = [[0,0,0], [0,1,0], [0,0,0]];
+      console.log(gameOfLife.display(previous));
+      var next = gameOfLife.next(previous);
+      console.log(gameOfLife.display(next));
+      expect(next).toEqual(expected);
+    });
+
+  });
+*/
 });
